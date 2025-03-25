c22600bdbf047ad5d62f3cf25d2852cc27ceab9e
Kylee Tilley <kyleetilley@gmail.com>
2025-03-25 13:12:40 -0500

    Sourcemaps in development not production

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: () => {
+    debugger;
     return <p>...</p>;
   },
 });
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,17 +4,26 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
   ],
   build: {
-    sourcemap: "inline",
+    sourcemap: mode === "development",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          tanstack: ["@tanstack/react-router", "@tanstack/react-query"],
+          mantine: ["@mantine/core", "@mantine/hooks"],
+        },
+      },
+    },
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
